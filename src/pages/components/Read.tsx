1fbaaacc5afe11263a5fb76298d93422b2ce8cf2
Divij <divijkathuria51714@gmail.com>
2023-06-02 20:40:41 +0530

    halka phulka kaam

--- src/pages/components/Read.tsx
+++ src/pages/components/Read.tsx
@@ -1,10 +1,18 @@
 import React from "react";
+import { Inter } from 'next/font/google';
+import { Roboto } from 'next/font/google';
+ 
+const roboto = Roboto({
+  weight: '900',
+  subsets: ['latin'],
+});
+const inter = Inter({ subsets: ['latin'] });
 
 export default function Read(){
     return(
-        <main  className="w-screen h-screen flex justify-center items-center bg-gradient-to-t from-trueGray-700 via-transparent to-yellow-600">
+        <main  className="w-screen h-screen flex bac2">
             <div className="flex flex-row">
-                <h1>What is</h1>
+                <h1 className={inter.className+ ` font-black text-8xl text-[#F8F2E9] `}>What is</h1>
                 <h1>SummerAI</h1>
             </div>
         </main>
