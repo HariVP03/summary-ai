f08e652439377e4907dde1a381a06d59977a9afe
Hari Vishnu Parashar <harryskotch11@gmail.com>
2023-06-02 14:58:34 +0530

    fix: bruh

--- src/pages/upload/index.tsx
+++ src/pages/upload/index.tsx
@@ -1,8 +1,28 @@
 import { Navbar } from "@/components/navbar";
-import { chakra } from "@chakra-ui/react";
+import {
+  Button,
+  Flex,
+  FormControl,
+  FormHelperText,
+  FormLabel,
+  Input,
+  chakra,
+} from "@chakra-ui/react";
 import Head from "next/head";
+import { useEffect, useState } from "react";
+import { YoutubeTranscript } from "youtube-transcript";
 
 export default function Upload() {
+  const [data, setData] = useState({});
+
+  useEffect(() => {
+    // fetch youtube video transcrupt
+
+    YoutubeTranscript.fetchTranscript("9JpdAg6uMXs").then((data) => {
+      setData(data);
+    });
+  }, []);
+
   return (
     <>
       <Head>
@@ -10,7 +30,26 @@ export default function Upload() {
       </Head>
       <chakra.main>
         <Navbar />
-        Upload
+        <Flex
+          flexDir="column"
+          justify="center"
+          align="center"
+          w="40%"
+          mt="64px"
+          mx="auto"
+        >
+          <FormControl>
+            <FormLabel>YouTube URL</FormLabel>
+            <Input placeholder="Give a valid link" type="url" />
+            <FormHelperText>
+              This is the YouTube video you want to summarise.
+            </FormHelperText>
+          </FormControl>
+
+          <Button colorScheme="blue" mt="8">
+            Start summarising 🚀
+          </Button>
+        </Flex>
       </chakra.main>
     </>
   );
